955fdf00d389ba82e659f9ab4c665d8afebc94c7
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-27 20:40:56 -0300

    Updates

---
+++ precache-manifest.4640dcada26bedb0d23a308cc13e6778.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69f7d5694396818c97ad63ca4678117",
+    "url": "/rs-goreact-m2/index.html"
+  },
+  {
+    "revision": "a84141a6331b45792ffa",
+    "url": "/rs-goreact-m2/static/css/2.9e538538.chunk.css"
+  },
+  {
+    "revision": "a84141a6331b45792ffa",
+    "url": "/rs-goreact-m2/static/js/2.f18ee338.chunk.js"
+  },
+  {
+    "revision": "9be840121fc2032f5e97",
+    "url": "/rs-goreact-m2/static/js/main.1217f64d.chunk.js"
+  },
+  {
+    "revision": "7cb7931580f50f22b15f",
+    "url": "/rs-goreact-m2/static/js/runtime~main.2071ec24.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rs-goreact-m2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rs-goreact-m2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rs-goreact-m2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rs-goreact-m2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rs-goreact-m2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
