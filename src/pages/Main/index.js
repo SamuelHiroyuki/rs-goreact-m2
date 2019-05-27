a18be16ca5b796212708db33c4af4dc320d9f257
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-26 21:10:17 -0300

    Font-awesome e loading

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,7 @@ import { FindRepository } from '../../services/api/Repository';
 
 class Main extends React.Component {
   state = {
+    loading: false,
     repositoryInput: '',
     repositoryError: false,
     repositories: [],
@@ -17,6 +18,9 @@ class Main extends React.Component {
 
   handleSubmit = async (e) => {
     e.preventDefault();
+
+    this.setState({ loading: true });
+
     const { repositories, repositoryInput } = this.state;
     try {
       const { data: repository } = await FindRepository(repositoryInput);
@@ -29,11 +33,18 @@ class Main extends React.Component {
       });
     } catch (error) {
       this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
     }
   };
 
   render() {
-    const { repositories, repositoryInput, repositoryError } = this.state;
+    const {
+      repositories,
+      repositoryInput,
+      repositoryError,
+      loading,
+    } = this.state;
     return (
       <Container>
         <img src={logo} alt="GITCOMPARE" />
@@ -45,7 +56,9 @@ class Main extends React.Component {
             onChange={e => this.setState({ repositoryInput: e.target.value })}
             placeholder="Usuário/Reposotório"
           />
-          <button type="submit">OK</button>
+          <button type="submit">
+            {loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
         </Form>
 
         <CompareList repositories={repositories} />
